51256854464b2b2759a55ab59fb8048a0613400e
Isaías De la Hoz <isaiasdelahoz@gmail.com>
2024-08-04 19:37:54 -0500

    add OrderGenerator spec

--- spec/services/order_generator_spec.rb
+++ spec/services/order_generator_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe OrderGenerator do
-  context '' do
+  context 'when params are valid' do
     let(:product) { create(:product, price: 10000) }
-
     let(:orders_params) do
       {
-        products: [{id: product.id, quantity: 2}], 
+        products: [{id: product.id, quantity: 2}],
         customer_info: {
           email: "email@example.com",
           name: "Customer name",
@@ -15,8 +14,10 @@ RSpec.describe OrderGenerator do
       }
     end
 
-    it '' do
-      response = described_class.new(orders_params).execute
+    it 'should create a new record' do
+      expect {
+        described_class.new(orders_params).execute
+      }.to change { Order.count }.by(1)
     end
   end
 end
